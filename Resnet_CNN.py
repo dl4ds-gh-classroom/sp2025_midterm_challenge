42f6dd1db8bbaf14404201f2ac9082bf66db86e5
Atul Aravind das <atuladas@scc1.bu.edu>
2025-03-26 00:47:45 -0400

    Commit M

---
+++ Resnet_CNN.py
@@ -0,0 +1,197 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import os
+import numpy as np
+import pandas as pd
+from tqdm.auto import tqdm  # For progress bars
+import wandb
+import torchvision.models as tv_models
+import json
+
+################################################################################
+# Model Definition (Simple Example - You need to complete)
+# For Part 2 you have the option of using a predefined network and
+def train(epoch, model, trainloader, optimizer, criterion, CONFIG):
+    device = CONFIG["device"]
+    model.train()  # Set the model to training mode
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    # put the trainloader iterator in a tqdm so it can printprogress
+    progress_bar = tqdm(trainloader, desc=f"Epoch {epoch+1}/{CONFIG['epochs']} [Train]", leave=False)
+
+    # iterate through all batches of one epoch
+    for i, (inputs, labels) in enumerate(progress_bar):
+
+        # move inputs and labels to the target device
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        ### TODO - Your code here
+        optimizer.zero_grad()
+        outputs=model(inputs)
+        loss=criterion(outputs,labels)
+
+
+        running_loss += loss  ### Add the losses
+        _, predicted = torch.max(outputs.data,1)
+
+        total += labels.size(0)
+        correct += predicted.eq(labels).sum().item()
+
+        progress_bar.set_postfix({"loss": running_loss / (i + 1), "acc": 100. * correct / total})
+
+    train_loss = running_loss / len(trainloader)
+    train_acc = 100. * correct / total
+    return train_loss, train_acc
+
+
+def validate(model, valloader, criterion, device):
+    model.eval() # Set to evaluation
+    running_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad(): # No need to track gradients
+        
+        # Put the valloader iterator in tqdm to print progress
+        progress_bar = tqdm(valloader, desc="[Validate]", leave=False)
+
+        # Iterate throught the validation set
+        for i, (inputs, labels) in enumerate(progress_bar):
+            
+            # move inputs and labels to the target device
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs) 
+            loss = criterion(outputs,labels)
+
+            running_loss += loss  
+            _, predicted = torch.max(outputs.data,1)   
+
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+
+            progress_bar.set_postfix({"loss": running_loss / (i+1), "acc": 100. * correct / total})
+
+    val_loss = running_loss/len(valloader)
+    val_acc = 100. * correct / total
+    return val_loss, val_acc
+
+
+def main():
+    CONFIG = {
+        "model": "ResNet_18_CNN",   # Change name when using a different model
+        "batch_size": 128, # run batch size finder to find optimal batch size
+        "learning_rate": 1e-4,
+        "epochs": 10,  # Train for longer in a real scenario
+        "num_workers": 2, # Adjust based on your system
+        "device": "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu",
+        "data_dir": "./data",  # Make sure this directory exists
+        "ood_dir": "./data/ood-test",
+        "wandb_project": "sp25-ds542-challenge",
+        "seed": 42,
+    }
+
+    import pprint
+    print("\nCONFIG Dictionary:")
+    pprint.pprint(CONFIG)
+
+    transform_train = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]), 
+    ])
+
+    
+    # Validation and test transforms (NO augmentation)
+    transform_test = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225]), 
+    ])
+
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
+                                            download=True, transform=transform_train)
+
+    
+    train_size = int(0.8*len(trainset))   
+    val_size = len(trainset)-train_size     
+    trainset, valset = torch.utils.data.random_split(trainset,[train_size,val_size])
+
+    
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=CONFIG["batch_size"],shuffle=True, num_workers=CONFIG["num_workers"])
+    valloader = torch.utils.data.DataLoader(valset, batch_size=CONFIG["batch_size"],shuffle=False, num_workers=CONFIG["num_workers"])
+
+    
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False,download=True, transform=transform_train)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=CONFIG["batch_size"],shuffle=False, num_workers=CONFIG["num_workers"])
+    
+    
+    model = tv_models.resnet18(pretrained=True)   
+    model = model.to(CONFIG["device"])   
+
+    print("\nModel summary:")
+    print(f"{model}\n")
+
+    SEARCH_BATCH_SIZES = False
+    if SEARCH_BATCH_SIZES:
+        from utils import find_optimal_batch_size
+        print("Finding optimal batch size...")
+        optimal_batch_size = find_optimal_batch_size(model, trainset, CONFIG["device"], CONFIG["num_workers"])
+        CONFIG["batch_size"] = optimal_batch_size
+        print(f"Using batch size: {CONFIG['batch_size']}")
+    
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=CONFIG["learning_rate"], weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.1)
+
+    wandb.init(project="-sp25-ds542-challenge", config=CONFIG)
+    wandb.watch(model)  # watch the model gradients
+
+    best_val_acc = 0.0
+
+    for epoch in range(CONFIG["epochs"]):
+        train_loss, train_acc = train(epoch, model, trainloader, optimizer, criterion, CONFIG)
+        val_loss, val_acc = validate(model, valloader, criterion, CONFIG["device"])
+        scheduler.step()
+
+        # log to WandB
+        wandb.log({
+            "epoch": epoch + 1,
+            "train_loss": train_loss,
+            "train_acc": train_acc,
+            "val_loss": val_loss,
+            "val_acc": val_acc,
+            "lr": optimizer.param_groups[0]["lr"] # Log learning rate
+        })
+
+        # Save the best model (based on validation accuracy)
+        if val_acc > best_val_acc:
+            best_val_acc = val_acc
+            torch.save(model.state_dict(), "best_model.pth")
+            wandb.save("best_model.pth") # Save to wandb as well
+
+    wandb.finish()
+
+    import eval_cifar100
+    import eval_ood
+
+    predictions, clean_accuracy = eval_cifar100.evaluate_cifar100_test(model, testloader, CONFIG["device"])
+    print(f"Clean CIFAR-100 Test Accuracy: {clean_accuracy:.2f}%")
+
+    all_predictions = eval_ood.evaluate_ood_test(model, CONFIG)
+
+    submission_df_ood = eval_ood.create_ood_df(all_predictions)
+    submission_df_ood.to_csv("submission_ood.csv", index=False)
+    print("submission_ood.csv created successfully.")
+
+if __name__ == '__main__':
+    main()
